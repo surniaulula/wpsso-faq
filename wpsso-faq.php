28fb976408ef3689fc7cdf109b31eb7f5075722a
JS Morisset <jsm@surniaulula.com>
2019-10-02 11:51:42 -0700

    v1.0.2-b.1

--- wpsso-faq.php
+++ wpsso-faq.php
@@ -13,7 +13,7 @@
  * Description: Manage FAQ categories with Question and Answer pages.
  * Requires At Least: 3.9
  * Tested Up To: 5.2.3
- * Version: 1.0.2-dev.3
+ * Version: 1.0.2-b.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
