e3ca4fd307d9c042522d3c0a0edb50340eec4cb2
JS Morisset <jsm@surniaulula.com>
2019-12-11 12:48:49 -0800

    v2.0.0

--- wpsso-faq.php
+++ wpsso-faq.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.5
  * Requires At Least: 3.9
  * Tested Up To: 5.3
- * Version: 2.0.0-rc.1
+ * Version: 2.0.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
