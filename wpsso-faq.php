78748a9baa68543ec994958f7704192a4c83cc20
JS Morisset <jsm@surniaulula.com>
2019-12-20 16:35:44 -0800

    v2.0.0

--- wpsso-faq.php
+++ wpsso-faq.php
@@ -12,7 +12,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: Manage FAQ categories with Question and Answer pages.
  * Requires PHP: 5.5
- * Requires At Least: 3.9
+ * Requires At Least: 4.0
  * Tested Up To: 5.3.2
  * Version: 2.0.0
  * 
