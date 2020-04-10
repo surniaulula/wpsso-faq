57c9724998d76fef612f69284927a395d39cba57
JS Morisset <jsm@surniaulula.com>
2020-04-10 12:46:11 -0700

    v3.0.0-dev.1

---
+++ lib/submenu/faq-general.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl.txt
+ * Copyright 2014-2020 Jean-Sebastien Morisset (https://wpsso.com/)
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'These aren\'t the droids you\'re looking for.' );
+}
+
+if ( ! class_exists( 'WpssoFaqSubmenuFaqGeneral' ) && class_exists( 'WpssoAdmin' ) ) {
+
+	class WpssoFaqSubmenuFaqGeneral extends WpssoAdmin {
+
+		public function __construct( &$plugin, $id, $name, $lib, $ext ) {
+
+			$this->p =& $plugin;
+
+			if ( $this->p->debug->enabled ) {
+				$this->p->debug->mark();
+			}
+
+			$this->menu_id   = $id;
+			$this->menu_name = $name;
+			$this->menu_lib  = $lib;
+			$this->menu_ext  = $ext;
+		}
+
+		/**
+		 * Called by the extended WpssoAdmin class.
+		 */
+		protected function add_meta_boxes() {
+
+			$metabox_id      = 'faq';
+			$metabox_title   = _x( 'FAQ Settings', 'metabox title', 'wpsso-faq' );
+			$metabox_screen  = $this->pagehook;
+			$metabox_context = 'normal';
+			$metabox_prio    = 'default';
+			$callback_args   = array(	// Second argument passed to the callback function / method.
+			);
+
+			add_meta_box( $this->pagehook . '_' . $metabox_id, $metabox_title,
+				array( $this, 'show_metabox_' . $metabox_id ), $metabox_screen,
+					$metabox_context, $metabox_prio, $callback_args );
+		}
+
+		public function show_metabox_faq() {
+
+			$metabox_id = 'faq-general';
+
+			$filter_name = SucomUtil::sanitize_hookname( $this->p->lca . '_' . $metabox_id . '_tabs' );
+
+			$tabs = apply_filters( $filter_name, array(
+				'shortcodes' => _x( 'Shortcode Defaults', 'metabox tab', 'wpsso-faq' ),
+			) );
+
+			$table_rows = array();
+
+			foreach ( $tabs as $tab_key => $title ) {
+
+				$filter_name = SucomUtil::sanitize_hookname( $this->p->lca . '_' . $metabox_id . '_' . $tab_key . '_rows' );
+
+				$table_rows[ $tab_key ] = array_merge(
+					$this->get_table_rows( $metabox_id, $tab_key ), 
+					(array) apply_filters( $filter_name, array(), $this->form )
+				);
+			}
+
+			$this->p->util->do_metabox_tabbed( $metabox_id, $tabs, $table_rows );
+		}
+
+		protected function get_table_rows( $metabox_id, $tab_key ) {
+
+			$table_rows = array();
+
+			switch ( $metabox_id . '-' . $tab_key ) {
+
+				case 'faq-general-shortcodes':
+
+					$table_rows[ 'faq_answer_text' ] = '' .
+					$this->form->get_th_html( _x( 'Question Answer Text', 'option label', 'wpsso-faq' ),
+						$css_class = '', $css_id = 'faq_answer_text' ) . 
+					'<td>' . $this->form->get_select( 'faq_answer_text', array(
+						'content' => __( 'Content Text', 'wpsso-faq' ),
+						'excerpt' => __( 'Excerpt', 'wpsso-faq' ),
+					) ) . '</td>';
+
+					break;
+			}
+
+			return $table_rows;
+		}
+	}
+}
