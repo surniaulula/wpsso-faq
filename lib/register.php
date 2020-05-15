49aa3cf054d10849556235249443c02521048b30
JS Morisset <jsm@surniaulula.com>
2020-05-15 10:32:57 -0700

    v3.3.0-dev.3

--- lib/register.php
+++ lib/register.php
@@ -108,7 +108,7 @@ if ( ! class_exists( 'WpssoFaqRegister' ) ) {
 				/**
 				 * Register plugin install, activation, update times.
 				 */
-				if ( class_exists( 'WpssoUtilReg' ) ) {	// Since WPSSO v6.13.1.
+				if ( class_exists( 'WpssoUtilReg' ) ) { // Since WPSSO Core v6.13.1.
 
 					$version = WpssoFaqConfig::$cf[ 'plugin' ][ 'wpssofaq' ][ 'version' ];
 
