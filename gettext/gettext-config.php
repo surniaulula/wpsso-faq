d3f23623747d978a9ec4fcd29a162ac9e3bbf1a7
JS Morisset <jsm@surniaulula.com>
2020-09-13 11:00:45 -0700

    v3.6.1

---
+++ gettext/gettext-config.php
@@ -0,0 +1,7 @@
+<?php die( 'These aren\'t the droids you\'re looking for.' );
+
+/**
+ * Extract of translatable strings from the config array.
+ */
+_x( 'FAQ Settings', 'lib file description', 'wpsso-faq' );
+_x( 'Create FAQ and Question / Answer Pages with Optional Shortcodes to Include FAQs and Questions in your Content.', 'plugin description', 'wpsso-faq' );
