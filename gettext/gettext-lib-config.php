7d97467f17ac7173542dcdbbc0c64fca0341b922
JS Morisset <jsm@surniaulula.com>
2020-09-15 09:15:33 -0700

    v3.6.1

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -4,4 +4,5 @@
  * Extract of translatable strings from lib/config.php.
  */
 _x( 'FAQ Settings', 'lib file description', 'wpsso-faq' );
+_x( 'WPSSO FAQ Manager', 'plugin name', 'wpsso-faq' );
 _x( 'Create FAQ and Question / Answer Pages with Optional Shortcodes to Include FAQs and Questions in your Content.', 'plugin description', 'wpsso-faq' );
