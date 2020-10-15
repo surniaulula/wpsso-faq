229f6d28aa26d0c7226c47148b1bf4bee805d7f9
JS Morisset <jsm@surniaulula.com>
2020-10-15 12:51:29 -0700

    v3.7.0-b.1

--- wpsso-faq.php
+++ wpsso-faq.php
@@ -64,21 +64,12 @@ if ( ! class_exists( 'WpssoFaq' ) ) {
 			return self::$instance;
 		}
 
-		public function init_textdomain( $debug_enabled = false ) {
+		public function init_textdomain() {
 
-			static $local_cache = null;
-
-			if ( null === $local_cache || $debug_enabled ) {
-
-				$local_cache = 'wpsso-faq';
-
-				load_plugin_textdomain( 'wpsso-faq', false, 'wpsso-faq/languages/' );
-			}
-
-			return $local_cache;
+			load_plugin_textdomain( 'wpsso-faq', false, 'wpsso-faq/languages/' );
 		}
 
-		public function init_objects() {
+		public function init_objects( $is_admin, $doing_ajax, $doing_cron ) {
 
 			$this->p =& Wpsso::get_instance();
 
