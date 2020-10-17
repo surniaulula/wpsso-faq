63b041d21cc8000a8c86c41707a615f4f0f2d474
JS Morisset <jsm@surniaulula.com>
2020-10-17 10:42:41 -0700

    v3.7.1

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoFaqConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssofaq' => array(			// Plugin acronym.
-					'version'     => '3.7.0',	// Plugin version.
+					'version'     => '3.7.1',	// Plugin version.
 					'opt_version' => '7',		// Increment when changing default option values.
 					'short'       => 'WPSSO FAQ',	// Short plugin name.
 					'name'        => 'WPSSO FAQ Manager',
