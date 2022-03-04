2ad65c3c3505f098a5f58d0dfd50804f0f27f820
JS Morisset <jsm@surniaulula.com>
2022-03-04 12:56:11 -0800

    v4.0.1-b.1

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoFaqConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssofaq' => array(			// Plugin acronym.
-					'version'     => '4.0.1-dev.7',	// Plugin version.
+					'version'     => '4.0.1-b.1',	// Plugin version.
 					'opt_version' => '9',		// Increment when changing default option values.
 					'short'       => 'WPSSO FAQ',	// Short plugin name.
 					'name'        => 'WPSSO FAQ Manager',
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoFaqConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '11.5.0-dev.7',
+							'min_version'   => '11.5.0-b.1',
 						),
 					),
 
