2f27a2527bde7acffac2b2e78c96de8dd71c93da
JS Morisset <jsm@surniaulula.com>
2022-10-11 06:47:08 -0700

    v4.1.0

--- wpsso-faq.php
+++ wpsso-faq.php
@@ -13,7 +13,7 @@
  * Description: Create FAQ and Question / Answer Pages with optional shortcodes to include FAQs and Questions / Answers in your content.
  * Requires PHP: 7.2
  * Requires At Least: 5.2
- * Tested Up To: 6.0.2
+ * Tested Up To: 6.1.0
  * Version: 4.1.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
