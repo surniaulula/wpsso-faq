36ac6165ab0a4b9ac18b50a1cf6b059f06d2643b
JS Morisset <jsm@surniaulula.com>
2022-12-28 12:03:05 -0800

    v4.1.0

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -1,8 +1,7 @@
 <?php die( 'These aren\'t the droids you\'re looking for.' );
-
-/**
- * Extract of translatable strings from lib/config.php.
- */
+// 
+// Extract of translatable strings from lib/config.php.
+// 
 _x( 'FAQ Settings', 'lib file description', 'wpsso-faq' );
 _x( 'WPSSO FAQ Manager', 'plugin name', 'wpsso-faq' );
 _x( 'Create FAQ and Question / Answer Pages with optional shortcodes to include FAQs and Questions / Answers in your content.', 'plugin description', 'wpsso-faq' );
