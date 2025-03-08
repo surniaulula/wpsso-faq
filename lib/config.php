cd9629cf6366776c59b658dfc97d3d2fa182dfde
jsmoriss <jsm@surniaulula.com>
2025-03-08 23:17:46 +0000

    v5.4.1-rc.1

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoFaqConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssofaq' => array(			// Plugin acronym.
-					'version'     => '5.4.1-dev.2',	// Plugin version.
+					'version'     => '5.4.1-rc.1',	// Plugin version.
 					'opt_version' => '10',		// Increment when changing default option values.
 					'short'       => 'WPSSO FAQ',	// Short plugin name.
 					'name'        => 'WPSSO FAQ Manager',
